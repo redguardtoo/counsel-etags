250ba92bd4b38c5c5e9cc72fd0f57db2d5f62d6d
Chen Bin <chenbin.sh@gmail.com>
2018-02-15 15:57:10 +1100

    documentation

--- counsel-etags.el
+++ counsel-etags.el
@@ -29,7 +29,7 @@
 ;;   "Exuberant Ctags" and "GNU Find" should exist at first.
 ;;
 ;;   "M-x counsel-etags-find-tag-at-point" to navigate.  This command will also
-;;   run `counsel-etags-scan-code' automatically if tags file is not built yet.
+;;   run `counsel-etags-scan-code' AUTOMATICALLY if tags file is not built yet.
 ;;
 ;;   "M-x counsel-etags-scan-code" to create tags file
 ;;   "M-x counsel-etags-grep" to grep
@@ -40,11 +40,11 @@
 ;; That's all!
 ;;
 ;; Tips:
-;; - Add below code into "~/.emacs" to auto-update scan code:
+;; - Add below code into "~/.emacs" to AUTOMATICALLY update tags file:
 ;;
-;;   ;; Don't ask before rereading the TAGS files if they have changed
+;;   ;; Don't ask before re-reading changed TAGS files
 ;;   (setq tags-revert-without-query t)
-;;   ;; Don't warn when TAGS files are large
+;;   ;; NO warning when loading large TAGS files
 ;;   (setq large-file-warning-threshold nil)
 ;;   (add-hook 'prog-mode-hook
 ;;     (lambda ()
